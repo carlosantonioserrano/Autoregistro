f8ddea6296ad1c71d0652f81e7c5193f1eed5c2d
Charly <35874093+carlosantonioserrano@users.noreply.github.com>
2022-05-08 13:52:32 -0600

    Create consulta_empleados.py

---
+++ consulta_empleados.py
@@ -0,0 +1,32 @@
+import mysql.connector
+# importarmos el conector
+
+# Iniciamos un try
+try:
+    #creamos la variable conn que es la conexion de mysql
+    conn = mysql.connector.connect(user='root', password='Pass123', host='localhost', database='empresa', port='3306')
+
+except Exception as err:
+    print('ERROR al conectar a la base de datos')
+
+else:
+    print('CONEXION A LA BDD SATISFACTORIA')
+
+# creamos un cursor
+cursor = conn.cursor()
+
+# creeamos la query
+sql = "SELECT * FROM empleados WHERE dui = '123457' "
+
+# ejecutamos la query
+cursor.execute(sql)
+
+# guardo la consulta en una variable llamada datos con el metodo fecthall
+datos = cursor.fetchall()
+
+#para recorrer la consulta uso for
+for fila in datos:
+    print(fila)
+
+# cerramos la conexion
+conn.close()
