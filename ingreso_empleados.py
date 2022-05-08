9536569f633aae369b02d24fc3cf2a6024390d65
Charly <35874093+carlosantonioserrano@users.noreply.github.com>
2022-05-08 00:59:01 -0600

    Create ingreso_empleados.py

---
+++ ingreso_empleados.py
@@ -0,0 +1,27 @@
+import mysql.connector
+# importarmos el conector
+
+# Iniciamos un try
+try:
+    #creamos la variable conexion de mysql
+    conexion = mysql.connector.connect(user='root', password='Pass123', host='localhost', database='empresa', port='3306')
+
+except Exception as err:
+    print('ERROR al conectar a la base de datos')
+
+else:
+    print('CONEXION A LA BDD SATISFACTORIA')
+
+# creamos un cursor
+cursor = conexion.cursor()
+
+# creeamos la query
+sql = "INSERT INTO empleados(nombres, apellidos, dui) VALUES('Will', 'Smith', '0123458')"
+
+# ejecutamos la query
+cursor.execute(sql)
+
+conexion.commit()
+
+# cerramos la conexion
+conexion.close()
